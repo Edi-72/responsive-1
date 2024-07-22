2ac99fc97bda7e82fc22a17ec1146414d38bda40
Edris <chrowhastyar@gmail.com>
2024-07-21 23:22:01 -0700

    projet nahaii baraie sherkat.commit first

---
+++ asset/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        ltr:true,
+        items: 4,
+        margin:0,
+        autoplay: false,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+    });
+  });
